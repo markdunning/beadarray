52cb4d8a383f50c7abe2aa00dbc372b1a9fdaf4f
unknown <mls40@cam.ac.uk>
2006-06-01 17:04:10 +0000

    Improved speed of findAllOutliers by writing it in C
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/beadarray@18272 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/findAllOutliers.R
+++ R/findAllOutliers.R
@@ -1,4 +1,4 @@
-findAllOutliers <- function(BLData, array, log = FALSE, n = 3, ignoreList=NULL){
+findAllOutliersSlow <- function(BLData, array, log = FALSE, n = 3, ignoreList=NULL){
   
   probes <- sort(unique(BLData$ProbeID[BLData$ProbeID[,array] > 0,array]))
 
@@ -10,3 +10,18 @@ findAllOutliers <- function(BLData, array, log = FALSE, n = 3, ignoreList=NULL){
   outliers
 }
 
+findAllOutliers <- function(BLData, array, log = FALSE, n = 3){
+
+  probes <- sort(unique(BLData$ProbeID[BLData$ProbeID[,array] > 0,array]))
+
+  finten <- BLData$R[,array]
+  probeList <- BLData$ProbeID[,array]
+  nbeads <- length(BLData$R[,array])
+
+  start = 0
+
+  foo <- .C("findAllOutliers", as.double(finten), binStatus = integer(length = nbeads), as.integer(probeList), as.integer(probes), as.integer(length(probes)), as.integer(nbeads), as.integer(start), PACKAGE = "beadarray")
+
+  which((probeList > 0) & (foo$binStatus == 0))
+
+}
