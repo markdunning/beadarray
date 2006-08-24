82b935c3f979189eaa6131f702af72c9060dc1b8
unknown <md392@cam.ac.uk>
2006-08-24 14:25:23 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/beadarray@19450 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/methods-ExpressionSetIllumina.R
@@ -0,0 +1,121 @@
+
+setMethod("initialize", "ExpressionSetIllumina",
+          function(.Object,
+                   phenoData = new("AnnotatedDataFrame"),
+                   experimentData = new("MIAME"),
+                   annotation = character(),
+		   exprs = new("matrix"),
+                   BeadStDev = new("matrix"),
+                   NoBeads = new("matrix"),
+                   Detection = new("matrix"),
+                   QC = new("matrix"),
+                   ... ) {
+            .Object<-callNextMethod(.Object,
+                           assayData = assayDataNew(
+			     exprs = exprs,
+                             BeadStDev = BeadStDev,
+                             NoBeads = NoBeads,
+                             Detection = Detection,
+                             ...),
+                           phenoData = phenoData,
+                           experimentData = experimentData,
+                           annotation = annotation,
+                           QC = QC
+                                    )
+            validObject(.Object)
+            .Object
+
+          })
+
+
+setMethod("[", "ExpressionSetIllumina", function(x, i, j, ..., drop = FALSE) {
+          x<-callNextMethod(x, i, j, ..., drop=drop)
+          if(!is.null(reporterInfo(x)) && !missing(i)) reporterInfo(x)<-reporterInfo(x)[i,, ..., drop = drop]
+          x
+})
+
+
+
+
+setMethod("exprs", c("ExpressionSetIllumina"), function(object) assayDataElement(object, "exprs"))
+
+setMethod("se.exprs", c("ExpressionSetIllumina"), function(object) assayDataElement(object, "BeadStDev"))
+
+
+
+setReplaceMethod("exprs", c("ExpressionSetIllumina", "matrix"), function(object, value) {
+  assayDataElementReplace(object, "exprs", value)
+})
+
+
+.mergeAssayData<-function(x, y, newdimnames) {
+  # this is derived from assayData combine method
+  # differences:
+  # - allows different number of reporters/features
+  # - will merge data from identical column names into 1 column ie rbind()) 
+  # - only works on 2-dimensional assayData elements
+  combineElement <- function(x, y) {
+    outarr<-array(NA,dim=c(length(newdimnames[[1]]),length(newdimnames[[2]])),newdimnames)
+    mode(outarr)<-mode(x)
+    outarr[rownames(y),colnames(y)]<-y
+    outarr[rownames(x),colnames(x)]<-x
+    outarr
+  }
+  storage.mode <- storageMode(x)
+  nmfunc <- assayDataElementNames
+
+  if (storageMode(y) != storage.mode)
+    stop(paste("assayData must have same storage, but are ",
+               storage.mode, ", ", storageMode(y), sep=""))
+  if (length(nmfunc(x)) != length(nmfunc(y)))
+    stop("assayData have different numbers of elements:\n\t",
+         paste(nmfunc(x), collapse=" "), "\n\t",
+         paste(nmfunc(y), collapse=" "))
+  if (!all(nmfunc(x) == nmfunc(y)))
+    stop(paste("assayData have different element names:",
+               paste(nmfunc(x), collapse=" "),
+               paste(nmfunc(y), collapse=" "), sep="\n\t"))
+               
+  for (nm in nmfunc(x)) {
+    x<-assayDataElementReplace(x,nm,combineElement(assayDataElement(x,nm),assayDataElement(y,nm)))
+  }
+  x
+}
+
+.mergePhenodata<-function(x , y, samples) {
+  variables<-union(colnames(pData(x)),colnames(pData(y)))
+  outarr<-array(data=NA,dim=c(length(samples),length(variables)),dimnames=list(samples,variables))
+  outarr[sampleNames(y),colnames(pData(y))]<-as.matrix(pData(y))
+  outarr[sampleNames(x),colnames(pData(x))]<-as.matrix(pData(x))
+  pd<-data.frame(outarr)
+  vardescs<-union(colnames(varMetadata(x)),colnames(varMetadata(y)))
+  outarr<-array(data=NA,dim=c(length(variables),length(vardescs)),dimnames=list(variables,vardescs))
+  outarr[colnames(pData(y)),colnames(varMetadata(y))]<-as.matrix(varMetadata(y))
+  outarr[colnames(pData(x)),colnames(varMetadata(x))]<-as.matrix(varMetadata(x))
+  vd<-data.frame(outarr)
+  new("AnnotatedDataFrame", data=pd, varMetadata=vd)
+}
+
+
+setMethod("combine", c("ExpressionSetIllumina", "ExpressionSetIllumina"), function(x, y, ...) {
+
+  if (class(x) != class(y))
+    stop(paste("objects must be the same class, but are ",
+               class(x), ", ", class(y), sep=""))
+  ## we need a kind of merge functionality in order to combine OPA panels
+  newdimnames<-list(union(featureNames(x),featureNames(y)),union(sampleNames(x),sampleNames(y)))
+  x <- .mergeAssayData(x, y, newdimnames)
+  # a bit of a hack to only keep the union, and discard double entries
+  phenoData(x) <- .mergePhenodata(x, y, newdimnames[[2]])
+  experimentData(x) <- combine(experimentData(x),experimentData(y))
+  reporterInfo(x)<-.mergeReporterInfo(reporterInfo(x), reporterInfo(y), newdimnames[[1]])
+    
+  ## annotation -- constant
+  if (any(annotation(x) != annotation(y))) {
+    warning("objects have different annotations: ",
+         annotation(x), ", ", annotation(y))
+    annotation(x)<-unique(c(annotation(x),annotation(y)))
+  }
+  x
+})
+
