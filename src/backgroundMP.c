4fcf1dbcdd3020d9c249c994de1735fdf2609e1f
Mike Smith <mls40@cam.ac.uk>
2009-10-30 14:35:54 +0000

    Testing some C code, may be removed shortly!
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/beadarray@42751 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/backgroundMP.c
+++ src/backgroundMP.c
@@ -1,14 +1,14 @@
-#define USE_RINTERNALS
-
 #include <R.h>
 #include <Rinternals.h>
 #include <Rdefines.h>
 #include <omp.h>
 
-void performCalc(int start, int end, int nbeads, int imageHeight, SEXP pixelMatrix, SEXP coords, double *bg) {
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+
+void performCalc(int start, int end, int nbeads, int imageHeight, SEXP pixelMatrix, SEXP coords, double *bg, int tid) {
     
     double x, y, newX, newY;
-    int i, j, k, count, M[289], tmp;
+    int i, j, k, count, tmp, M[289];
     
     for(i = start; i < end; i++) {
     
@@ -37,22 +37,22 @@ void performCalc(int start, int end, int nbeads, int imageHeight, SEXP pixelMatr
        for(k = start2; k <= end2; k++ ) {
         M[count++] = INTEGER(pixelMatrix)[tmp + k];
       }
-    }
+    } 
 
     /* use R's internal sorting algorithm */
     R_qsort_int(M, 1, 289);
 
     bg[i] = (M[0] + M[1] + M[2] + M[3] + M[4]) / 5.0;
     }
-}
-/*
+} 
+
 SEXP backgroundMP(SEXP pixelMatrix, SEXP coords) {
 
   SEXP background;
   int imageWidth, imageHeight, nbeads, i, j, k;
   double x, y, newX, newY, *bg;
-  int tmp; 
-  int nthreads, tid;
+  int tmp, start, end; 
+  int tid, nthreads, numProcs;
     
   imageHeight = INTEGER(getAttrib(pixelMatrix, R_DimSymbol))[0];
   imageWidth = INTEGER(getAttrib(pixelMatrix, R_DimSymbol))[1];
@@ -61,52 +61,37 @@ SEXP backgroundMP(SEXP pixelMatrix, SEXP coords) {
   PROTECT(background = allocVector(REALSXP, nbeads));
   bg = REAL(background);
 
-  omp_set_num_threads(2);
- 
-  #pragma omp parallel default(shared) private(nthreads, tid)
+  /* find the number of processors and set the number of threads 
+  currently has a maximum value of 4 */
+  #if defined (_OPENMP)
+  numProcs = min(omp_get_num_procs(), 4);
+  omp_set_num_threads(numProcs);
+  #endif
+  
+  /* initialize the background vector with zeros, can probably be removed */
+  for(i = 0; i < nbeads; i++) { bg[i] == 0; }
+  
+  #pragma omp parallel shared(nthreads, nbeads, imageHeight, pixelMatrix, coords) private(tid, start, end)
   {
-    Rprintf("Hello World from thread = %d\n", tid);
-    if (tid == 0) {
-        performCalc(0, nbeads/2, nbeads, imageHeight, pixelMatrix, coords, bg);
-    }
-    else {
-        performCalc(nbeads/2, nbeads, nbeads, imageHeight, pixelMatrix, coords, bg);
-    }
+      
+      #if defined (_OPENMP)
+      nthreads = omp_get_num_threads();
+      tid = omp_get_thread_num();
+      #else
+      nthreads = 1;
+      tid = 0;
+      #endif
+      
+    start = (int) floor(tid * ((double)nbeads / (double)nthreads));
+    end = (int) floor((tid + 1) * ((double)nbeads / (double)nthreads));
+    Rprintf("%d\t%d\t%d\t%d\t%d\n", tid, nbeads, nthreads, start, end);
+    
+    performCalc(start, end, nbeads, imageHeight, pixelMatrix, coords, bg, tid);
+
   }
   UNPROTECT(1);
   return(background);
-} */
-
-void silly(int i) {
- 
-    int nthreads, tid;
-    
-//  #pragma omp parallel default(shared) private(nthreads, tid)
-//{
-    Rprintf("Hello World from thread = %d\n", tid);
-//  }
-}
-
-SEXP backgroundMP(SEXP pixelMatrix, SEXP coords) {
-
-  SEXP background;
-  int imageWidth, imageHeight, nbeads, i, j, k;
-  double x, y, newX, newY, *bg;
-  int tmp; 
-  int nthreads, tid;
-    
-  imageHeight = INTEGER(getAttrib(pixelMatrix, R_DimSymbol))[0];
-  imageWidth = INTEGER(getAttrib(pixelMatrix, R_DimSymbol))[1];
-
-  nbeads = INTEGER(getAttrib(coords, R_DimSymbol))[0];
-
-  omp_set_num_threads(2);
-  PROTECT(background = allocVector(REALSXP, nbeads));
-  bg = REAL(background);
-  for(i = 0; i < nbeads; i++) { bg[i] == 0; }
-
-  //  silly(4);
-
-  UNPROTECT(1);
-  return(background);
 } 
+
+
+
