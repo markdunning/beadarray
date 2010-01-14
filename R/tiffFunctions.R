fd99eed6ed0208d6893dd9f5061b8c0810a85392
Mike Smith <mls40@cam.ac.uk>
2010-01-14 18:25:16 +0000

    Added support for reading compressed tif images.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/beadarray@44012 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tiffFunctions.R
+++ R/tiffFunctions.R
@@ -1,39 +1,72 @@
-readTIFF <- function(fileName, verbose = FALSE) {
-  #open the connection
-  con <- file(fileName, "rb");
-  #read the tiff header information
-  mode1 <- readBin(con, integer(), 1, size = 1);
-  mode2 <- readBin(con, integer(), 1, size = 1);
-  version <- readBin(con, integer(), 1, size = 2);
-  offset <- readBin(con, integer(), 1, size = 4);
-  seek(con, offset);
-  records <- readBin(con, integer(), 1, size = 2);
-  if(verbose) #print information if required
-    cat("Mode1:", mode1, "\nMode2:", mode2, "\nVersion:", version, "\nOffset", offset, "\nRecords", records, "\n")
-  #loop over each of the records in the header
-  for(i in 1:records) {
-    tag = readBin(con, integer(), 1, size = 2);
-    type = readBin(con, integer(), 1, size = 2);
-    length = readBin(con, integer(), 1, size = 4);
-    offset <- readBin(con, integer(), 1, size = 4);
-    #store the useful information
-    if(tag == 256)
-      ImageWidth = offset
-    if(tag == 257)
-      ImageHeight = offset
-    if(tag == 273)
-      StripOffset = offset
+openTIFF <- function(fileName, fileExt)
+{
+# Provides support for reading the tiff files in compressed formats such as bz2 and gz
+# Support for zipped tifs may be added later
+    
+    con <- switch(fileExt,
+                  "tif" = file(fileName, "rb"),
+                  "bz2" = bzfile(fileName, "rb"),
+                  "gz" = gzfile(fileName, "rb")               
+                  )   
+    return(con)
+}
 
-    if(verbose)
-      cat("Tag:", tag, "Type:", type, "Length:", length, "Offset:", offset, "\n")
-  }
-  seek(con, StripOffset);
-  #read the rest of the image in one go, it's much faster that way!
-  data <- readBin(con, integer(), n = ImageWidth * ImageHeight, size = 2, signed = FALSE);
-  #rearrange the data into the matrix
-  data <- t(matrix(data, ncol = ImageHeight, nrow = ImageWidth));
-  close(con);
-  return(data);
+readTIFF <- function(fileName, verbose = FALSE) {
+
+    # determine the file extension
+    fileExt <- unlist(strsplit(fileName, "\\."))
+    fileExt <- fileExt[length(fileExt)]
+    
+    # open the connection
+    con <- openTIFF(fileName, fileExt);
+
+    # read the tiff header information
+    mode1 <- readBin(con, integer(), 1, size = 1);
+    mode2 <- readBin(con, integer(), 1, size = 1);
+    version <- readBin(con, integer(), 1, size = 2);
+    offset <- readBin(con, integer(), 1, size = 4);
+
+    # we can only use "seek" if the file isn't in a compressed format
+    if(fileExt == "tif")
+        seek(con, offset)
+    else
+        tmp <- readBin(con, integer(), size = 1, n = (offset - 8) )
+
+    records <- readBin(con, integer(), 1, size = 2);
+    if(verbose) #print information if required
+        cat("Mode1:", mode1, "\nMode2:", mode2, "\nVersion:", version, "\nOffset", offset, "\nRecords", records, "\n")
+    # loop over each of the records in the header
+    for(i in 1:records) {
+        tag = readBin(con, integer(), 1, size = 2);
+        type = readBin(con, integer(), 1, size = 2);
+        length = readBin(con, integer(), 1, size = 4);
+        offset <- readBin(con, integer(), 1, size = 4);
+    # store the useful information
+        if(tag == 256)
+            ImageWidth = offset
+        if(tag == 257)
+            ImageHeight = offset
+        if(tag == 273)
+            StripOffset = offset
+        
+        if(verbose)
+            cat("Tag:", tag, "Type:", type, "Length:", length, "Offset:", offset, "\n")
+    }
+
+    if(fileExt == "tif")
+        seek(con, StripOffset)
+    else {
+        close(con);
+        con <- openTIFF(fileName, fileExt);
+        tmp <- readBin(con, integer(), size = 1, n = StripOffset)
+    }
+    
+    # read the rest of the image in one go, it's much faster that way!
+    data <- readBin(con, integer(), n = ImageWidth * ImageHeight, size = 2, signed = FALSE);
+    # rearrange the data into the matrix
+    data <- t(matrix(data, ncol = ImageHeight, nrow = ImageWidth));
+    close(con);
+    return(data);
 }
 
 plotTIFF <- function(tiff, xstart = 0, xend = ncol(tiff)-1, ystart = 0, yend = nrow(tiff)-1, high = "cyan", low = "black", mid = NULL, ncolours = 100, log = TRUE, values = FALSE, textCol = "black", accountForZero = FALSE, ...) {
