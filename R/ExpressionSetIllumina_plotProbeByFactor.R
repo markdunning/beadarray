902e55cf06341438a7e7dc3a2eb1e7f388674258
unknown <md392@cam.ac.uk>
2011-09-26 08:43:57 +0000

    Fix to plotProbeByFactor
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/beadarray@58376 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ExpressionSetIllumina_plotProbeByFactor.R
+++ R/ExpressionSetIllumina_plotProbeByFactor.R
@@ -1,6 +1,5 @@
 plotByProbeFactor <- function(data, probeFactor = "PROBEQUALITY"){
 
-	require("reshape")
 	require("ggplot2")
 
 	if(is.character(probeFactor)){
@@ -13,7 +12,6 @@ plotByProbeFactor <- function(data, probeFactor = "PROBEQUALITY"){
 
 	newDf <- melt(data.frame(exprs(data), ProbeFactor = probeFactor), id.vars ="ProbeFactor")
 
-	#	newDf <- newDf[-which(is.na(newDf$mostGs)),]
 	
 	p<- ggplot(newDf, aes(x=factor(ProbeFactor),y=value,fill=factor(ProbeFactor))) + geom_boxplot() + facet_wrap(~variable,ncol=min(dim(data)[2], 6))
 
