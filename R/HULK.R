697ba2923b09f544315992478297e7b483a2e46c
Mike Smith <mls40@cam.ac.uk>
2011-10-21 10:35:04 +0000

    Updated HULK to deal with a single array at a time.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/beadarray@59490 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/HULK.R
+++ R/HULK.R
@@ -1,64 +1,53 @@
-HULK <- function(BLData, array = 1, neighbours = NULL, invasions = 20, useLocs = TRUE, weightName ="wts", transFun = logGreenChannelTransform) {
+HULK <- function(BLData, array = 1, neighbours = NULL, invasions = 20, useLocs = TRUE, weightName ="wts", transFun = logGreenChannelTransform, outlierFun = illuminaOutlierMethod) {
 
-  
-    an <- sectionNames(BLData)
-    tmp = BLData[[array]]
-    probeIDs = tmp[,"ProbeID"]
+    message(paste("HULKing section", sectionNames(BLData)[array]))
+
+    if(is.null(neighbours)) {
+        message("Calculating Neighbourhood")
+        neighbours <- generateNeighbours(BLData, array, useLocs=useLocs)
+    }
+
     data <- transFun(BLData, array)
 
-    out = NULL	
-
-    for(i in array) {
-    #print something encouraging
-        cat("Array",i,":\n")
-
-        if(is.null(neighbours)) {
-            cat("Calculating Neighbourhood\n")
-            neighbours <- generateNeighbours(BLData, i, useLocs=useLocs)
-        }
-
-        out[[i]] = data - HULKResids(BLData, i, transFun, useLocs, neighbours, invasions, weightName = weightName)
-    }
-    out
+    return( data - HULKResids(BLData, array = array, transFun = transFun, outlierFun = outlierFun, useLocs, neighbours, invasions, weightName = weightName) )
 }
 
-HULKResids <- function(BLData, array, transFun = logGreenChannelTransform, useLocs = TRUE, neighbours = NULL, invasions = 20, weightName = "wts") {
+HULKResids <- function(BLData, array, transFun = logGreenChannelTransform, outlierFun = illuminaOutlierMethod, useLocs = TRUE, neighbours = NULL, invasions = 20, weightName = "wts") {
 
 	
-    tmp = BLData[[array]]
-
-    probeIDs = tmp[,"ProbeID"]
+    probeIDs = getBeadData(BLData, array = array, what = "ProbeID")
             
-    if(weightName %in% colnames(tmp)){
-        weights <- tmp[,"wts"]
+    if(weightName %in% colnames(BLData[[array]])){
+        weights <- getBeadData(BLData, array = array, what = weightName)
     }
     else {
         weights = rep(1, length(probeIDs))
     }
-            
-    an <- sectionNames(BLData)
-
-    probeIDs = tmp[,"ProbeID"]
 
     data <- transFun(BLData, array)
 
    ###Remove outliers first	
 
-    outliers = illuminaOutlierMethod(data, probeIDs)
-
+    outliers = outlierFun(data, probeIDs, wts = weights)
     	
+    if(length(outliers)) {
+        beadTypeMeans = lapply(split(data[-outliers], probeIDs[-outliers]), mean, na.rm=TRUE)
+    }
+    else {
+        beadTypeMeans = lapply(split(data, probeIDs), mean, na.rm=TRUE)
+    }
 
-    beadTypeMeans = lapply(split(data[-outliers], probeIDs[-outliers]), mean, na.rm=TRUE)
 
+    ## if an entire bead-type is missing (e.g. all weights 0) then the residuals will all be NA
     residuals = data - unlist(beadTypeMeans)[as.character(probeIDs)]
+    ## beads with a residual of 0 are ignored
     residuals[which((is.na(residuals)) | (weights == 0))] = 0
     
     if(is.null(neighbours)) {
-        cat("Calculating Neighbourhood\n")
+        message("Calculating Neighbourhood\n")
         neighbours <- generateNeighbours(BLData, array, useLocs = useLocs)
     }
-    
-    cat("HULKing\n")      
+       
     output <- .C("HULK", as.double(residuals), as.integer(t(neighbours)), as.integer(nrow(neighbours)), as.integer(invasions), results = as.double(residuals))
     output$results
 }
