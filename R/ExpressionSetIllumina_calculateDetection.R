aff1f064f879802cfb00cdae4cdc09f4722a6645
Mark Dunning <Mark.Dunning@cancer.org.uk>
2014-02-25 16:21:59 +0000

    Add progress bar to calculateDetection
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/beadarray@86783 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ExpressionSetIllumina_calculateDetection.R
+++ R/ExpressionSetIllumina_calculateDetection.R
@@ -1,35 +1,37 @@
 calculateDetection = function(BSData, status=fData(BSData)$Status, negativeLabel="negative"){
-
-detScores = matrix(nrow=nrow(exprs(BSData)),ncol=ncol(exprs(BSData)))
- 
-##Use the fData to find which the negative IDS are 
-
-negInd = which(status == negativeLabel)
-
-
-detect= function(x) 1 - (sum(x>negvals)/(length(negvals)))
-
-
-	for(i in 1:ncol(exprs(BSData))){
-
-		negvals = exprs(BSData)[negInd,i]
-
-		M = median(negvals, na.rm=TRUE)
-		MAD = mad(negvals, na.rm=TRUE)
-
-		negvals = negvals[negvals < M+3*MAD]
-		negvals = negvals[!is.na(negvals)]
-
-		detScores[,i] = sapply(exprs(BSData)[,i], detect)
-
-	}
-
-colnames(detScores) =sampleNames(BSData)
-rownames(detScores) = featureNames(BSData)
-
-
-detScores
-
+  
+  detScores = matrix(nrow=nrow(exprs(BSData)),ncol=ncol(exprs(BSData)))
+  
+  ##Use the fData to find which the negative IDS are 
+  
+  negInd = which(status == negativeLabel)
+  
+  
+  detect= function(x) 1 - (sum(x>negvals)/(length(negvals)))
+  
+  pb <- txtProgressBar(min=1,max=ncol(exprs(BSData)),style=3)
+  
+  
+  for(i in 1:ncol(exprs(BSData))){
+    
+    negvals = exprs(BSData)[negInd,i]
+    
+    M = median(negvals, na.rm=TRUE)
+    MAD = mad(negvals, na.rm=TRUE)
+    
+    negvals = negvals[negvals < M+3*MAD]
+    negvals = negvals[!is.na(negvals)]
+    
+    detScores[,i] = sapply(exprs(BSData)[,i], detect)
+    setTxtProgressBar(pb, i)
+    
+  }
+  
+  colnames(detScores) =sampleNames(BSData)
+  rownames(detScores) = featureNames(BSData)
+  
+  
+  detScores
+  
 }
-
 
